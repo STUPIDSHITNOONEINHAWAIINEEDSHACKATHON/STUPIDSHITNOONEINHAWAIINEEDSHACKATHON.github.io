a2d714ecb6b220ffe8e604745796905543653192
sgnl <rayrfarias@gmail.com>
2016-10-21 22:46:43 -1000

    update giphy link to https

--- js/app.js
+++ js/app.js
@@ -43,7 +43,7 @@ window.onload = function() {
         body.appendChild(imgElement);
       });
 
-      req.open("GET", `http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=${randomTag}`);
+      req.open("GET", `https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=${randomTag}`);
       return req.send();
     }
 
